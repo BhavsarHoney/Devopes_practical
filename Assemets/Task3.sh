b1dc04ece7925e58bcb6936442db17be1e929d2f
BhavsarHoney <124352103+BhavsarHoney@users.noreply.github.com>
2024-02-12 11:53:24 +0530

    Create Task3.sh

---
+++ Assemets/Task3.sh
@@ -0,0 +1,16 @@
+read -p "ENter your NAme ": name
+
+if [ "$name" == "abc" ]
+then
+  echo "NAme is Right"
+  read "Enter the number": num
+     if [ "$num" == "123" ]
+     then
+      echo "NUmber is right"
+     fi
+elif [ "$name" == "abc" $$ "$num" == "123" ]
+  echo "name and numbr is correct"
+
+else 
+   echo "plese enter valid name"
+fi
